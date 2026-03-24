11673dd0e9b2286f703f305a7987856a6ee7a010
Kalyani Chitre <kalyani.chitre@sjsu.edu>
2026-03-24 15:52:15 -0700

    add cot_3shot to inference init

--- src/inference/__init__.py
+++ src/inference/__init__.py
@@ -2,5 +2,6 @@
 
 from src.inference.test_inference import dummy_inference
 from src.inference.base_inference import BaseInference
+from src.inference.cot_3shot import cot_3shot
 
-__all__ = ["dummy_inference", "BaseInference"]
+__all__ = ["dummy_inference", "BaseInference", "cot_3shot"]
